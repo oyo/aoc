da76fdeac0875fb4f94d26b09000d27c4a84d184
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2020-12-02 11:43:37 +0100

    aoc-2020 day 02

--- src/2020/01/puzzle.js
+++ src/2020/01/puzzle.js
@@ -1,27 +1,25 @@
 const _ = require('lodash');
 
-const puzzle = {
+const P = {
 
-    prep: (input) => _.map(input.split('\n'), line => parseInt(line)),
+    prep: T => _.map(T.split('\n'), L => parseInt(L)),
 
-    part_1: (input) => {
-        const p = puzzle.prepa(input) 
-        const r = _.max(p.flatMap(i => 
+    part_1: T => {
+        const p = P.prep(T) 
+        return _.max(p.flatMap(i => 
             p.map(j => i + j === 2020 ? i * j : 0 )
         ))
-        return r
     },
 
-    part_2: (input) => {
-        const p = puzzle.prep(input) 
-        const r = _.max(p.flatMap(i => 
+    part_2: T => {
+        const p = P.prep(T) 
+        return _.max(p.flatMap(i => 
             p.flatMap(j => 
                 p.map(k => i + j + k === 2020 ? i * j * k : 0 )
             )
         ))
-        return r
     }
 
 }
 
-exports.puzzle = puzzle
+exports.puzzle = P
--- src/2020/02/puzzle.js
+++ src/2020/02/puzzle.js
@@ -1,16 +1,16 @@
 const P = {
 
-    prep: t => t.split('\n').map(l => l.split(/[- :]/)),
+    prep: T => T.split('\n').map(L => L.split(/[- :]/)),
 
-    match_1: l => new RegExp('^'+l[2]+'{'+l[0]+','+l[1]+'}$').test(
-        l[4].replace(new RegExp('[^'+l[2]+']*', 'g'), '')
+    match_1: L => new RegExp('^'+L[2]+'{'+L[0]+','+L[1]+'}$').test(
+        L[4].replace(new RegExp('[^'+L[2]+']*', 'g'), '')
     ),
 
-    match_2: l => (l[4].charAt(l[0]*1-1) === l[2]) ^ (l[4].charAt(l[1]*1-1) === l[2]),
+    match_2: L => (L[4][L[0]*1-1] === L[2]) ^ (L[4][L[1]*1-1] === L[2]),
 
-    part_1: t => P.prep(t).filter(l => P.match_1(l)).length,
+    part_1: T => P.prep(T).filter(L => P.match_1(L)).length,
 
-    part_2: t => P.prep(t).filter(l => P.match_2(l)).length
+    part_2: T => P.prep(T).filter(L => P.match_2(L)).length
 
 }
 
