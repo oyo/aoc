05651590a463aee8c20171a83ef71c4338c7a01f
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2020-12-03 11:15:12 +0100

    20/03 cleanup

--- src/2020/03/puzzle.js
+++ src/2020/03/puzzle.js
@@ -1,7 +1,5 @@
 const P = {
 
-    prep: T => T.split('\n'),
-
     slope: (R, d) => {
         let count = 0
         let x = -d[0]
@@ -9,10 +7,10 @@ const P = {
         return count
     },
 
-    part_1: T => P.slope(P.prep(T), [3,1]),
+    part_1: T => P.slope(T.split('\n'), [3,1]),
 
     part_2: T => [ [1,1], [3,1], [5,1], [7,1], [1,2] ]
-        .map( s => P.slope(P.prep(T), s) )
+        .map( s => P.slope(T.split('\n'), s) )
         .reduce( (a, b) => a * b )
 
 }
