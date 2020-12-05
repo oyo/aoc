5df0dbcee25bee2a533af8f9d33a31d6a7980f8f
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2020-12-05 12:01:11 +0100

    20/05 eliminated lodash

--- src/2020/05/puzzle.js
+++ src/2020/05/puzzle.js
@@ -1,12 +1,10 @@
-const _ = require('lodash')
-
 const P = {
 
-    prep: T => T.replace(/[FL]/g,'0').replace(/[BR]/g,'1').split('\n').sort() .map(S => parseInt(S,2)),
+    prep: T => T.replace(/[FL]/g,'0').replace(/[BR]/g,'1').split('\n').sort().map(s => parseInt(s,2)),
 
-    part_1: T => _.max(P.prep(T)),
+    part_1: T => Math.max(...P.prep(T)),
 
-    part_2: T => _.max(_.pullAll([...Array(P.part_1(T)).keys()],P.prep(T)))
+    part_2: T => Math.max(...(a => a.slice(1).map((n,i) => n-a[i]===2 ? a[i] : 0))(P.prep(T)))+1
 
 }
 
