37ed0820ec427cb0295a90f5ed6916cf89410e1f
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2020-12-07 11:38:15 +0100

    20/07 restructuring

--- src/2020/07/puzzle.js
+++ src/2020/07/puzzle.js
@@ -10,32 +10,33 @@ const P = {
     prep_2: T => T.split('\n').map(L => L
         .split(/(s? contain )|(s?, )|(s?\.)/)
         .filter(b => b && b.match(/^.*bag$/) && !b.match(/no other/))
-        .flatMap(b => [b.substring(0,b.length-4)]
-            .flatMap(c => c.match(/^\d+ /) ? new Array(c.substring(0,2).trim()*1).fill(c.substring(2).trim()) : c)
-        )),
-
-    part_1: T => {
-        const p = P.prep_1(T)
-        let t = ['shiny gold']
-        let u = []
-        let all = false
-        while (!all) {
-            u = p.filter(a => _.intersection(t,a).length>0).map(a => a[0])
-            all = t.length === u.length
-            t = _.union(t,u)
-        }
-        return t.length-1
-    },
-
-    getBagsRek: (B, n) => B[n].length + B[n].reduce((a,b) => a + P.getBagsRek(B, b), 0),
-
-    part_2: T => P.getBagsRek(
-        P.prep_2(T).reduce((b,a) => {
+        .map(b => b.substring(0,b.length-4))
+        .flatMap(b => b.match(/^\d+ /)
+            ? new Array(b.substring(0,2).trim()*1).fill(b.substring(2).trim())
+            : b
+        ))
+        .reduce((b,a) => {
             b[ a.shift() ] = a
             return b
         },{}),
-        'shiny gold'
-    )
+
+    canFindIn: (B, n) => {
+        let t = [n]
+        let u = []
+        let all = false
+        while (!all) {
+            u = B.filter(a => _.intersection(t,a).length>0).map(a => a[0])
+            all = t.length === u.length
+            t = _.union(t,u)
+        }
+        return t
+    },
+
+    countRek: (B, n) => B[n].length + B[n].reduce((a,b) => a + P.countRek(B, b), 0),
+
+    part_1: T => P.canFindIn(P.prep_1(T), 'shiny gold').length - 1,
+
+    part_2: T => P.countRek(P.prep_2(T), 'shiny gold')
 
 }
 
