fe478cf08a422630c0d64bbcfe3c1e8f6090f41f
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2020-12-10 08:17:02 +0100

    20/10 direct array

--- src/2020/10/puzzle.js
+++ src/2020/10/puzzle.js
@@ -1,12 +1,10 @@
 const P = {
 
-    G: [1, 1, 2, 4, 7],
-
     prep: T => (s => s.map((a, i) => i === 0 ? a : a - s[i - 1]))(T.split('\n').map(L => L * 1).sort((a, b) => a - b)),
 
     part_1: T => (d => (d.filter(a => a === 1).length * (d.filter(a => a === 3).length + 1)))(P.prep(T)),
 
-    part_2: T => P.prep(T).join('').split('3').reduce((a, b) => a * P.G[b.length], 1)
+    part_2: T => P.prep(T).join('').split('3').reduce((a, b) => a * [1, 1, 2, 4, 7][b.length], 1)
 
 }
 
