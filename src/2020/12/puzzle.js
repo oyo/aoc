fa9eba741e07438bf1f6392d273fb38c09531125
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2020-12-12 14:48:03 +0100

    20/12 functional style

--- src/2020/12/puzzle.js
+++ src/2020/12/puzzle.js
@@ -17,16 +17,14 @@ const P = {
         dy: 1
     },
 
-    part_1: T => {
-        const p = P.prep(T)
-        const pos = p.reduce((a,m) => {
+    part_1: T => (pos => Math.abs(pos.x) + Math.abs(pos.y))(
+        P.prep(T).reduce((a,m) => {
             const v = m[1]
             switch (m[0]) {
                 case 'N': a.y += v; break
                 case 'S': a.y -= v; break
                 case 'E': a.x += v; break
                 case 'W': a.x -= v; break
-//                case 'L': a.a = (a.a-v+360) % 360; break
                 case 'R': a.a = (a.a+v) % 360; break
                 case 'F':
                     const w = (90-a.a)*(Math.PI/180)
@@ -34,14 +32,10 @@ const P = {
                     a.y += Math.round(v * Math.sin(w))
             }
             return a
-        }, P.start)
-        return Math.abs(pos.x) + Math.abs(pos.y)
-    },
+        }, P.start)),
 
-    part_2: T => {
-        const p = P.prep(T)
-        //return p 
-        const pos = p.reduce((a,m) => {
+    part_2: T => (pos => Math.abs(pos.x) + Math.abs(pos.y))(
+        P.prep(T).reduce((a,m) => {
             const v = m[1]
             switch (m[0]) {
                 case 'N': a.dy += v; break
@@ -72,14 +66,9 @@ const P = {
                 case 'F':
                     a.x += v * a.dx;
                     a.y += v * a.dy;
-                    break
-                default: return 'ERROR'
             }
-            //console.log(a)
             return a
-        }, P.startWP)
-        return Math.abs(pos.x) + Math.abs(pos.y)
-    }
+        }, P.startWP))
 
 }
 
