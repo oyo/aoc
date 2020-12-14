876294da870e02d69fb603ed22f068459f3033d8
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2020-12-14 16:21:08 +0100

    20/14 code cleanup

--- src/2020/14/puzzle.js
+++ src/2020/14/puzzle.js
@@ -4,15 +4,15 @@ const P = {
 
     prep: T => T.split('\n').map(L => L.split(/ = /)),
 
-    value: (v,m) => (BigInt(v) |
+    value: (v, m) => (BigInt(v) |
         BigInt('0b' + m.replace(/X/g, '0'))) &
         BigInt('0b' + m.replace(/X/g, '1')),
 
-    addresses: (v,m) => {
-        let list = [ BigInt(v) | BigInt('0b' + m.replace(/X/g, '0')) ]
+    addresses: (v, m) => {
+        let list = [BigInt(v) | BigInt('0b' + m.replace(/X/g, '0'))]
         for (let i = 0; i < m.length; i++) {
-            if (m[35-i]==='X') {
-                const smask = BigInt(1)<<BigInt(i)
+            if (m[35 - i] === 'X') {
+                const smask = BigInt(1) << BigInt(i)
                 list = list.flatMap(a => [
                     a | smask,
                     a & (~smask)
@@ -22,29 +22,21 @@ const P = {
         return list.map(a => a.toString(2))
     },
 
-    part_1: T => {
+    run: (T, f) => {
         let mask = 0
-        let memory = {}
+        let mem = {}
         P.prep(T).forEach(l => {
             if (l[0] === 'mask')
                 mask = l[1]
             else
-                memory[l[0].split(/[\[\]]/g)[1]] = P.value(l[1], mask)
+                f(mem, mask, l[0].split(/[\[\]]/g)[1], l[1])
         })
-        return _.reduce(memory, (a,v) => a + BigInt(v), BigInt(0)).toString()*1
+        return _.reduce(mem, (a, v) => a + BigInt(v), BigInt(0)).toString() * 1
     },
 
-    part_2: T => {
-        let mask = 0
-        let memory = {}
-        P.prep(T).forEach(l => {
-            if (l[0] === 'mask')
-                mask = l[1]
-            else
-                P.addresses(l[0].split(/[\[\]]/g)[1], mask).forEach(a => memory[a] = BigInt(l[1]))
-        })
-        return _.reduce(memory, (a,v) => a + BigInt(v), BigInt(0)).toString()*1
-    }
+    part_1: T => P.run(T, (mem, mask, adr, val) => mem[adr] = P.value(val, mask)),
+
+    part_2: T => P.run(T, (mem, mask, adr, val) => P.addresses(adr, mask).forEach(a => mem[a] = BigInt(val)))
 
 }
 
