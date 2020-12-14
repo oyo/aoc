85d1720ce941edc5f73c25f1aa7bf639fa389c4e
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2020-12-14 22:52:17 +0100

    20/14 fixed broken tests

--- src/2020/14/puzzle.test.js
+++ src/2020/14/puzzle.test.js
@@ -8,17 +8,16 @@ const INPUT = [
     'mem[8] = 0'
 ].join('\n')
 
-
 const MASK_26 = '00000000000000000000000000000000X0XX'
 const OUT_26 = [
-    '10000',
-    '10001',
-    '10010',
-    '10011',
-    '11000',
-    '11001',
-    '11010',
-    '11011',
+    16n,
+    17n,
+    18n,
+    19n,
+    24n,
+    25n,
+    26n,
+    27n,
 ].sort()
 
 const INPUT_2 = [
@@ -37,7 +36,6 @@ it ('masks values', () => {
 it ('masks addresses', () => {
     expect(puzzle.addresses(26, MASK_26).sort()).toEqual(OUT_26)
 })
-
 
 it ('solves part_1', () => {
     expect(puzzle.part_1(INPUT)).toEqual(165)
