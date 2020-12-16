768e0ca392f85acdf1ba34ada450bfefa34faf8e
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2020-12-16 08:43:43 +0100

    20/16 code cleanup

--- src/2020/16/puzzle.js
+++ src/2020/16/puzzle.js
@@ -2,64 +2,59 @@ const _ = require('lodash')
 
 const P = {
 
-    prep: T => T.split(/\n+(your|nearby) tickets?:\n+/).map(L => L.split('\n')),
+    prep: T => (p => ({
+        rules: P.prepRules(p[0]),
+        my: P.prepTicket(p[2])[0],
+        tickets: P.prepTicket(p[4])
+    }))(
+        T.split(/\n+(your|nearby) tickets?:\n+/).map(L => L.split('\n'))
+    ),
 
     prepRules: R => R.map(l => l.split(/(-|: | or )/)),
-    prepTicket: R => R.map(l => l.split(/,/)),
 
-    valid: (t,r) => t.reduce((a,v) => {
-        let c =  
-            (r.reduce((a,f) => a ||
-                (v*1 >= f[2]*1 && v*1 <= f[4]*1) ||
-                (v*1 >= f[6]*1 && v*1 <= f[8]*1), false))
+    prepTicket: R => R.map(l => l.split(/,/).map(a => a * 1)),
+
+    transpose: m => m[0].map((x, i) => m.map(x => x[i] * 1)),
+
+    errors: (t, r) => t.reduce((b, v) => b +
+        ((r.reduce((a, f) => a ||
+            (v >= f[2] * 1 && v <= f[4] * 1) ||
+            (v >= f[6] * 1 && v <= f[8] * 1), false))
                 ? 0
-                : v*1
-            return a+c
-        },
+                : v),
         0),
 
-    matrix: (f,tt) => {
-        const result = tt.reduce((b,ti,i) => {
-            let c = (ti.reduce((a,v) => a &&
-                    ((v*1 >= f[2]*1 && v*1 <= f[4]*1) ||
-                    (v*1 >= f[6]*1 && v*1 <= f[8]*1)), true))
+    matchIndex: (f, tt) => {
+        const result = tt.reduce((b, ti, i) => {
+            let c = (ti.reduce((a, v) => a &&
+                ((v >= f[2] * 1 && v <= f[4] * 1) ||
+                    (v >= f[6] * 1 && v <= f[8] * 1)), true))
             if (c)
                 b.push(i)
             return b
-        },[])
+        }, [])
         return result
     },
 
-    part_1: T => {
-        const p = P.prep(T)
-        const rules = P.prepRules(p[0])
-        const tickets = P.prepTicket(p[4])
-        const valid = tickets.reduce((a,t) => a + P.valid(t,rules),0)
-        return valid
-    },
+    part_1: T => (p => p.tickets.reduce((a, t) => a + P.errors(t, p.rules), 0))(P.prep(T)),
 
     part_2: T => {
         const p = P.prep(T)
-        const rules = P.prepRules(p[0])
-        const my = P.prepTicket(p[2])[0]
-        const tickets = P.prepTicket(p[4])
-        const filter = tickets.filter(t => P.valid(t,rules)===0)
-        const transpose = m => m[0].map((x,i) => m.map(x => x[i]*1))
-        const tt = transpose(filter)
-        let ri = rules.map(r => [r[0], P.matrix(r, tt)])
-        const rulesindex = new Array(rules.length)
-        while (ri.length>0) {
-            let risf = ri.filter(r => r[1].length===1)
-            risf.forEach(r => {
-                const idx = r[1][0]
-                rulesindex[idx]=r[0]
-                ri = ri.map(y => [y[0], y[1].filter(fi => fi !== idx)])
-            })
-            ri = ri.filter(r => r[1].length>0)
+        const transValids = P.transpose(p.tickets.filter(t => P.errors(t, p.rules) === 0))
+        let ri = p.rules.map(r => [r[0], P.matchIndex(r, transValids)])
+        const rulesindex = new Array(p.rules.length)
+        while (ri.length > 0) {
+            ri.filter(r => r[1].length === 1)
+                .forEach(r => {
+                    const idx = r[1][0]
+                    rulesindex[idx] = r[0]
+                    ri = ri.map(y => [y[0], y[1].filter(fi => fi !== idx)])
+                })
+            ri = ri.filter(r => r[1].length > 0)
         }
-        const myticket = rulesindex.map((r,i) => [r,my[i]])
-        const result = myticket.reduce((a,r) => a * (r[0].match(/^departure /) ? r[1] : 1),1)
-        return result
+        return rulesindex
+            .map((r, i) => [r, p.my[i]])
+            .reduce((a, r) => a * (r[0].match(/^departure /) ? r[1] : 1), 1)
     }
 
 }
