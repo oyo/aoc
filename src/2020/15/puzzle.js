5760a8f40589ffecf8925723566cf8c48b28722f
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2020-12-16 20:04:36 +0100

    formatting

--- src/2020/15/puzzle.js
+++ src/2020/15/puzzle.js
@@ -1,25 +1,25 @@
 const P = {
 
-    run: (T, n) => {
-	const input = T.split(',')
-	const record = new Array(30000000)
-	const process = (number, index) => {
-		const first = record[number] === undefined
-		const seen = record[number]
-		record[number] = index
-		return first ? 0 : index - seen
-	}
-	let last = 0
-	for (let i = 0; i < input.length; i++)
-		last = process(input[i], i)
-	for (let i = input.length; i < n - 1; i++)
-		last = process(last, i)
-        return last
-    },
+	run: (T, n) => {
+		const p = T.split(',')
+		const mem = new Array(30000000)
+		const process = (number, index) => {
+			const first = mem[number] === undefined
+			const seen = mem[number]
+			mem[number] = index
+			return first ? 0 : index - seen
+		}
+		let last = 0
+		for (let i = 0; i < p.length; i++)
+			last = process(p[i], i)
+		for (let i = p.length; i < n - 1; i++)
+			last = process(last, i)
+		return last
+	},
 
-    part_1: T => P.run(T, 2020),
+	part_1: T => P.run(T, 2020),
 
-    part_2: T => P.run(T, 30000000)
+	part_2: T => P.run(T, 30000000)
 
 }
 
