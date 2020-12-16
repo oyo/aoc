0a22226061565724d5c11a4dda8cf1c837689828
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2020-12-16 09:49:06 +0100

    test fixes

--- src/2020/15/puzzle.js
+++ src/2020/15/puzzle.js
@@ -1,10 +1,11 @@
 const P = {
 
     run: (T, n) => {
+        const p = T.split(',')
         const mem = new Map()
-        T.split(',').forEach((n, i) => mem.set(n * 1, [i + 1]))
-        let last = mem[6]
-        for (let i = 7; i < n; i++) {
+        p.forEach((n, i) => mem.set(n * 1, [i + 1]))
+        let last = mem[p.length-1]
+        for (let i = p.length; i < n; i++) {
             const num = mem.has(last)
                 ? i - mem.get(last)
                 : 0
--- src/2020/16/puzzle.test.js
+++ src/2020/16/puzzle.test.js
@@ -18,8 +18,8 @@ const INPUT = [
 
 const INPUT_2 = [
     'class: 0-1 or 4-19',
-    'row: 0-5 or 8-19',
-    'seat: 0-13 or 16-19',
+    'departure row: 0-5 or 8-19',
+    'departure seat: 0-13 or 16-19',
     '',
     'your ticket:',
     '11,12,13',
@@ -30,10 +30,24 @@ const INPUT_2 = [
     '5,14,9',
 ].join('\n')
 
+it ('prepares input', () => {
+    const p1 = puzzle.prep(INPUT)
+    const p2 = puzzle.prep(INPUT_2)
+    expect(p1.tickets.length).toEqual(4)
+    expect(p2.tickets.length).toEqual(3)
+})
+
 it ('solves part_1', () => {
     expect(puzzle.part_1(INPUT)).toEqual(71)
 })
 
+it ('calculates matching indices', () => {
+    const p = puzzle.prep(INPUT_2)
+    expect(puzzle.matchIndex(p.rules[0], p.tickets)).toEqual([1,2])
+    expect(puzzle.matchIndex(p.rules[1], p.tickets)).toEqual([0,1,2])
+    expect(puzzle.matchIndex(p.rules[2], p.tickets)).toEqual([2])
+})
+
 it ('solves part_2', () => {
-    expect(puzzle.part_2(INPUT_2)).toEqual(1)
+    expect(puzzle.part_2(INPUT_2)).toEqual(11*13)
 })
