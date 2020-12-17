de1e554206e25203392352f3850407228864cf64
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2020-12-17 19:33:36 +0100

    20/17 code cleanup

--- src/2020/17/puzzle.js
+++ src/2020/17/puzzle.js
@@ -1,5 +1,7 @@
 const P3 = {
 
+	prep: T => T.split('\n'),
+
 	prepEmpty: d => {
 		const za = new Array(d + 2)
 		for (let z = 0; z < d + 2; z++) {
@@ -24,7 +26,9 @@ const P3 = {
 		return board
 	},
 
-	prep: T => T.split('\n'),
+	clone: (z) => z.slice().map(y => y.slice().map(x => x.slice())),
+
+	count: (z) => z.reduce((b, y) => b + y.reduce((a, x) => a + x.filter(c => c === '#').length,0),0),
 
 	adjacentCount: (p, z, y, x) =>
 		(p[z - 1][y - 1][x - 1] === '#' ? 1 : 0) +
@@ -55,38 +59,18 @@ const P3 = {
 		(p[z + 1][y + 1][x] === '#' ? 1 : 0) +
 		(p[z + 1][y + 1][x + 1] === '#' ? 1 : 0),
 
-	step: (board, counter, transposer) => {
-		board.push(board.shift())
-		const p = board[0]
-		const p1 = board[1]
+	step: (counter, transposer) => {
+		P3.board.push(P3.board.shift())
+		const p = P3.board[0]
+		const p1 = P3.board[1]
 		for (let z = 1; z < P3.D.z - 1; z++)
 			for (let y = 1; y < P3.D.y - 1; y++)
 				for (let x = 1; x < P3.D.x - 1; x++)
-					p[z][y][x] = transposer(p1[z][y][x], counter(p1, z, y, x))
+                    p[z][y][x] = transposer(p1[z][y][x], counter(p1, z, y, x))
+        return P3.count(p)
 	},
 
-	clone: (board) => board.slice().map(y => y.slice().map(x => x.slice())),
-
-	//countx: (board) => board.reduce((a, z) => a + z.reduce((a, y) => a + y.filter(c => c === '#')),0,0).length,
-	count: (board) => {
-		let c = 0
-		for (let z = 1; z < P3.D.z - 1; z++)
-			for (let y = 1; y < P3.D.y - 1; y++)
-				for (let x = 1; x < P3.D.x - 1; x++)
-					if (board[z][y][x] === '#')
-						c++
-		return c
-	},
-
-	renderstep: (counter, transposer) => {
-		P3.stepN++
-        P3.step(P3.board, counter, transposer)
-        const count = P3.count(P3.board[0])
-        //console.log(P3.stepN + ' ' + count)
-        return count
-	},
-
-	part_step: () => P3.renderstep(P3.adjacentCount, (s, c) =>
+	part_step: () => P3.step(P3.adjacentCount, (s, c) =>
 		s === '#' && (c < 2 || c > 3)
 			? '.'
 			: (
@@ -97,8 +81,7 @@ const P3 = {
 	),
 
 	run: T => {
-		const p = P3.prep(T)
-		let board = P3.populate(22, p)
+		const board = P3.populate(22, P3.prep(T))
 		P3.orig = board
 		P3.D = {
 			z: board.length - 2,
@@ -106,7 +89,6 @@ const P3 = {
 			x: board[0][0].length - 2
 		}
 		P3.board = [P3.clone(P3.orig), P3.clone(P3.orig)]
-		P3.stepN = 0
         let count = 0
         for (let i=0; i<6; i++)
             count = P3.part_step()
@@ -116,6 +98,8 @@ const P3 = {
 }
 
 const P4 = {
+
+	prep: T => T.split('\n'),
 
 	prepEmpty: d => {
 		const za = new Array(d + 2)
@@ -145,7 +129,9 @@ const P4 = {
 		return board
 	},
 
-	prep: T => T.split('\n'),
+	clone: (z) => z.slice().map(y => y.slice().map(x => x.slice().map(w => w.slice()))),
+
+	count: (z) => z.reduce((a, y) => a + y.reduce((b, x) => b + x.reduce((c, w) => c + w.filter(c => c === '#').length,0),0),0),
 
 	adjacentCount: (p, z, y, x, w) =>
         (p[z - 1][y - 1][x - 1][w - 1] === '#' ? 1 : 0) +
@@ -232,40 +218,19 @@ const P4 = {
         (p[z + 1][y + 1][x    ][w + 1] === '#' ? 1 : 0) +
         (p[z + 1][y + 1][x + 1][w + 1] === '#' ? 1 : 0),
 
-	step: (board, counter, transposer) => {
-		board.push(board.shift())
-		const p = board[0]
-		const p1 = board[1]
+	step: (counter, transposer) => {
+		P4.board.push(P4.board.shift())
+		const p = P4.board[0]
+		const p1 = P4.board[1]
 		for (let z = 1; z < P4.D.z - 1; z++)
 			for (let y = 1; y < P4.D.y - 1; y++)
 				for (let x = 1; x < P4.D.x - 1; x++)
 	    			for (let w = 1; w < P4.D.w - 1; w++)
-					    p[z][y][x][w] = transposer(p1[z][y][x][w], counter(p1, z, y, x, w))
+                        p[z][y][x][w] = transposer(p1[z][y][x][w], counter(p1, z, y, x, w))
+        return P4.count(p)
 	},
 
-	clone: (z) => z.slice().map(y => y.slice().map(x => x.slice().map(w => w.slice()))),
-
-	//countx: (board) => board.reduce((a, z) => a + z.reduce((a, y) => a + y.filter(c => c === '#')),0,0).length,
-	count: (board) => {
-		let c = 0
-		for (let z = 1; z < P4.D.z - 1; z++)
-			for (let y = 1; y < P4.D.y - 1; y++)
-				for (let x = 1; x < P4.D.x - 1; x++)
-    				for (let w = 1; w < P4.D.w - 1; w++)
-					    if (board[z][y][x][w] === '#')
-					    	c++
-		return c
-	},
-
-	renderstep: (counter, transposer) => {
-		P4.stepN++
-        P4.step(P4.board, counter, transposer)
-        const count = P4.count(P4.board[0])
-        //console.log(P4.stepN + ' ' + count)
-        return count
-	},
-
-	part_2_step: () => P4.renderstep(P4.adjacentCount, (s, c) =>
+	part_step: () => P4.step(P4.adjacentCount, (s, c) =>
 		s === '#' && (c < 2 || c > 3)
 			? '.'
 			: (
@@ -276,8 +241,7 @@ const P4 = {
 	),
 
     run: T => {
-        const p = P4.prep(T)
-        let board = P4.populate(22, p)
+        const board = P4.populate(22, P4.prep(T))
         P4.orig = board
         P4.D = {
             z: board.length - 2,
@@ -286,10 +250,9 @@ const P4 = {
             w: board[0][0][0].length - 2
         }
         P4.board = [P4.clone(P4.orig), P4.clone(P4.orig)]
-        P4.stepN = 0
         let count = 0
         for (let i=0; i<6; i++)
-            count = P4.part_2_step()
+            count = P4.part_step()
         return count
     }
 
