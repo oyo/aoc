8244165eb55ef725d47bca9acffc4e5409e1c8a0
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2020-12-18 12:12:35 +0100

    20/18 performance

--- src/2020/18/puzzle.js
+++ src/2020/18/puzzle.js
@@ -2,31 +2,36 @@ const P = {
 
     prep: T => T.replace(/ /g, '').split('\n'),
 
+    sub: {
+        paren: /\(([0-9+*]+)\)/,
+        ltor: /^(\d+([+*])\d+)/,
+        add: /(\d+[+]\d+)/,
+        mul: /(\d+[*]\d+)/
+    },
+
     eval1: e => {
         let g
-        const rg = /\(([0-9+*]+)\)/
-        while (g = e.match(rg))
-            e = e.replace(rg, P.eval1(g[1]))
-        let o
-        const ro = /^(\d+[+*]\d+)/
-        while (o = e.match(ro))
-            e = e.replace(ro, eval(o[1]))
+        while (g = e.match(P.sub.paren))
+            e = e.replace(P.sub.paren, P.eval1(g[1]))
+        while (g = e.match(P.sub.ltor)) {
+            const t = g[1].split(/[+*]/)
+            e = e.replace(P.sub.ltor, g[2]==='+' ? 1*t[0]+1*t[1] : t[0]*t[1])
+        }
         return e * 1
     },
 
     eval2: e => {
         let g
-        const rg = /\(([0-9+*]+)\)/
-        while (g = e.match(rg))
-            e = e.replace(rg, P.eval2(g[1]))
-        let a
-        const ra = /(\d+[+]\d+)/
-        while (a = e.match(ra))
-            e = e.replace(ra, eval(a[1]))
-        let m
-        const rm = /(\d+[*]\d+)/
-        while (m = e.match(rm))
-            e = e.replace(rm, eval(m[1]))
+        while (g = e.match(P.sub.paren))
+            e = e.replace(P.sub.paren, P.eval1(g[1]))
+        while (g = e.match(P.sub.add)) {
+            const t = g[1].split('+')
+            e = e.replace(P.sub.add, 1*t[0]+1*t[1])
+        }
+        while (g = e.match(P.sub.mul)) {
+            const t = g[1].split('*')
+            e = e.replace(P.sub.mul, t[0]*t[1])
+        }
         return e * 1
     },
 
