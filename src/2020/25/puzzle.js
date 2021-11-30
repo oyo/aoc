3a17f0be8531cc439512356863d277b1c6e75af1
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2021-11-30 11:34:45 +0100

    preparations for 2021

--- src/2020/25/puzzle.js
+++ src/2020/25/puzzle.js
@@ -2,23 +2,23 @@ const _ = require('lodash')
 
 const P = {
 
-    prep: T => T.split('\n').map(L => L*1),
+    prep: T => T.split('\n').map(L => L * 1),
 
     loops: pk => {
         let i = 0
-        for (let v=1, s=7; (pk-v) !== 0; i++)
+        for (let v = 1, s = 7; (pk - v) !== 0; i++)
             v = (v * s) % 20201227
         return i
     },
 
-    key: (s,l) => {
+    key: (s, l) => {
         let v = 1
         for (let i = 0; i < l; i++)
             v = (v * s) % 20201227
         return v
     },
 
-    part_1: T => (p => P.key(p[0],P.loops(p[1])))(P.prep(T)), // or P.key(p[1],l0)
+    part_1: T => (p => P.key(p[0], P.loops(p[1])))(P.prep(T)), // or P.key(p[1],l0)
 
     part_2: T => 0
 
