f159f6a479e86c9640f5679592eec4dae0804a2c
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2021-12-03 15:27:39 +0100

    2021/03 optimizations

--- src/2021/03/puzzle.js
+++ src/2021/03/puzzle.js
@@ -1,33 +1,29 @@
-const _ = require('lodash')
-
 exports.puzzle = P = {
 
     prep: T => {
         const l = T.split('\n')
-        P.N = l.length
         P.L = l[0].length
         return l.map(L => parseInt(L, 2))
     },
 
-    part_1: T => {
-        const p = P.prep(T)
-        const c = new Array(P.L).fill(0)
-        for (let i = 0; i < P.N; i++) {
-            const n = p[i]
-            for (let j = 0; j < P.L; j++) {
-                c[j] += (n >> (P.L - j - 1)) & 1
-            }
-        }
-        const b = c.map(d => d > P.N / 2 ? '1' : '0').join('')
-        const g = parseInt(b, 2)
-        const e = ~g & (Math.pow(2, P.L) - 1)
-        return g * e
-    },
+    countBits: (p, i) => p.reduce((c, n) => {
+        c[(n >> (P.L - i - 1)) & 1]++
+        return c
+    }, [0, 0]),
 
     divideByBit: (p, i) => p.reduce((c, n) => {
         c[(n >> (P.L - i - 1)) & 1].push(n)
         return c
     }, [[], []]).sort((a, b) => a.length - b.length),
+
+    part_1: T => {
+        const g = (p => (new Array(P.L).fill(0))
+            .map((x, i) => P.countBits(p, i))
+            .reduce((o, a, i) => {
+                return a[0] > a[1] ? o : (o | (1 << (P.L - i - 1)))
+            }, 0))(P.prep(T))
+        return g * (~g & (Math.pow(2, P.L) - 1))
+    },
 
     part_2: T => P.divideByBit(P.prep(T), 0).map((d, k) => {
         for (let i = 1; d.length > 1; i++)
