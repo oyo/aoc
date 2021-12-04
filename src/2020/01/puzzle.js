b4fc4377fcba336eab150abe7d00793d2d8ed081
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2021-12-04 17:53:05 +0100

    2020 code style updates

--- src/2020/01/puzzle.js
+++ src/2020/01/puzzle.js
@@ -1,33 +1,29 @@
-const _ = require('lodash');
+exports.puzzle = P = {
 
-const puzzle = {
+    prep: T => T.split('\n').map(L => L * 1),
 
-    prepare: (input) => _.map(input.split('\n'), line => parseInt(line)),
-
-    part_1: (input) => {
-        const p = puzzle.prepare(input)
+    part_1: T => {
+        const p = P.prep(T)
         let found = false;
         let a = 0;
         let b = 0;
-        for (let i=0; i<p.length && !found; i++)
-            for (let j=i+1; j<p.length && !found; j++)
-                found = (a=p[j]) + (b=p[i]) === 2020
+        for (let i = 0; i < p.length && !found; i++)
+            for (let j = i + 1; j < p.length && !found; j++)
+                found = (a = p[j]) + (b = p[i]) === 2020
         return a * b
     },
 
-    part_2: (input) => {
-        const p = puzzle.prepare(input)
+    part_2: T => {
+        const p = P.prep(T)
         let found = false;
         let a = 0;
         let b = 0;
         let c = 0;
-        for (let i=0; i<p.length && !found; i++)
-            for (let j=i+1; j<p.length && !found; j++)
-                for (let k=j+1; k<p.length && !found; k++)
-                    found = (a=p[i]) + (b=p[j]) + (c=p[k]) === 2020
+        for (let i = 0; i < p.length && !found; i++)
+            for (let j = i + 1; j < p.length && !found; j++)
+                for (let k = j + 1; k < p.length && !found; k++)
+                    found = (a = p[i]) + (b = p[j]) + (c = p[k]) === 2020
         return a * b * c
     }
 
 }
-
-exports.puzzle = puzzle
--- src/2020/03/puzzle.js
+++ src/2020/03/puzzle.js
@@ -1,18 +1,16 @@
-const P = {
+exports.puzzle = P = {
 
     slope: (R, d) => {
         let count = 0
         let x = -d[0]
-        R.forEach( (r,y) => count += !(y%d[1]) && r[x=(x+d[0])%r.length] === '#' ? 1 : 0 )
+        R.forEach((r, y) => count += !(y % d[1]) && r[x = (x + d[0]) % r.length] === '#' ? 1 : 0)
         return count
     },
 
-    part_1: T => P.slope(T.split('\n'), [3,1]),
+    part_1: T => P.slope(T.split('\n'), [3, 1]),
 
-    part_2: T => [ [1,1], [3,1], [5,1], [7,1], [1,2] ]
+    part_2: T => [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
         .map(s => P.slope(T.split('\n'), s))
-        .reduce((a,b) => a*b)
+        .reduce((a, b) => a * b)
 
 }
-
-exports.puzzle = P
