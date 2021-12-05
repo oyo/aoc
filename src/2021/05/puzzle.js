782aed0c3c3eb0d16c773f91c135394f24702654
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2021-12-05 09:24:02 +0100

    2021/05 simplified

--- src/2021/05/puzzle.js
+++ src/2021/05/puzzle.js
@@ -1,7 +1,5 @@
 exports.puzzle = P = {
 
-    DIM: 1000,
-
     prep: T => T.split('\n')
         .map(L => L.split(/ -> /))
         .map(L => [
@@ -9,31 +7,27 @@ exports.puzzle = P = {
             L[1].split(',').map(c => c * 1)
         ]),
 
-    clear: () => {
-        P.board = new Array(P.DIM).fill(0).map(() => new Array(P.DIM).fill(0));
-        return P
-    },
-
     toString: () => P.board.map(y => y.join('')).join('\n').replaceAll('0', '.'),
 
     draw: c => {
         const b = P.board
-        const y1 = c[0][1]
-        const y2 = c[1][1]
-        const x1 = c[0][0]
-        const x2 = c[1][0]
-        for (let x = x1, y = y1; ;) {
+        const y0 = c[0][1]
+        const y1 = c[1][1]
+        const x0 = c[0][0]
+        const x1 = c[1][0]
+        for (let x = x0, y = y0; ;) {
             b[y][x]++
-            if (x === x2 && y === y2) break;
-            x += x1 === x2 ? 0 : (x2 > x1 ? 1 : -1)
-            y += y1 === y2 ? 0 : (y2 > y1 ? 1 : -1)
+            if (x === x1 && y === y1) break
+            x += x0 === x1 ? 0 : (x1 > x0 ? 1 : -1)
+            y += y0 === y1 ? 0 : (y1 > y0 ? 1 : -1)
         }
     },
 
     count: () => P.board.reduce((o, a) => o + a.reduce((o, b) => o + (b > 1 ? 1 : 0), 0), 0),
 
     part_1: T => {
-        P.clear().prep(T).filter(c => c[0][0] === c[1][0] || c[0][1] === c[1][1]).map(c => P.draw(c))
+        P.board = new Array(1000).fill(0).map(() => new Array(1000).fill(0))
+        P.prep(T).filter(c => c[0][0] === c[1][0] || c[0][1] === c[1][1]).map(c => P.draw(c))
         return P.count()
     },
 
