88a31348d4158144b21589d6100f77836c3832cb
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2021-12-07 15:32:37 +0100

    2021/07 simplified

--- src/2021/07/puzzle.js
+++ src/2021/07/puzzle.js
@@ -1,9 +1,11 @@
+const {abs, min, max} = Math
+
 exports.puzzle = P = {
 
-    fuel: (T, f) => (p => Math.min(...new Array(Math.max(...p)).fill().map((_, h) => p.reduce((s, d) => s + f(Math.abs(h - d)), 0))))(T.split(',').map(n => n * 1)),
+    fuel: (T, f) => (p => min(...new Array(max(...p)).fill().map((_, h) => p.reduce((s, d) => s + f(abs(h - d)), 0))))(T.split(',').map(n => n * 1)),
 
     part_1: T => P.fuel(T, n => n),
 
-    part_2: T => P.fuel(T, n => (n * (n + 1)) / 2)
+    part_2: T => P.fuel(T, n => n * ++n >> 1)
 
 }
