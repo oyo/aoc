7533765cdcb5a5d1ab0846457fed3ab4d0fa5562
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2021-12-08 10:52:11 +0100

    2021/08 test code beauty

--- src/2021/08/puzzle.test.js
+++ src/2021/08/puzzle.test.js
@@ -1,5 +1,7 @@
 const { puzzle } = require('./puzzle')
 
+const LINE = `acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf`
+const MAPPING = ['d', 'e', 'a', 'f', 'g', 'b', 'c']
 const INPUT =
     `be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
 edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
@@ -12,17 +14,9 @@ bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbg
 egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
 gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce`
 
-const LINE = `acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf`
-const MAPPING = ['d', 'e', 'a', 'f', 'g', 'b', 'c']
-
 it('solves 1', () => expect(P.part_1(INPUT)).toEqual(26))
-
 it('finds mappings', () => expect(P.findSegmentMapping(P.prep(LINE)[0][0])).toEqual(MAPPING))
-
 it('finds digit', () => expect(P.findDigit(P.prep(LINE)[0][0][1], MAPPING)).toEqual(5))
-
 it('finds code', () => expect(P.findCode(P.prep(LINE)[0])).toEqual(5353))
-
 it('solves 2', () => expect(P.part_2(LINE)).toEqual(5353))
-
 it('solves 2', () => expect(P.part_2(INPUT)).toEqual(61229))
