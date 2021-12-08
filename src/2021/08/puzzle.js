c0e76d64cb67c0b146b30de2b26d6a819b5b30ac
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2021-12-08 10:39:25 +0100

    2021/08 optimizations

--- src/2021/08/puzzle.js
+++ src/2021/08/puzzle.js
@@ -1,34 +1,27 @@
-const _ = require('lodash')
-
 exports.puzzle = P = {
 
     prep: T => T.split('\n').map(L => L.split(' | ').map(p => p.split(' '))),
 
-    part_1: T => T.split('\n').map(L => L.split(' | ')[1].split(' ').map(s => s.length).filter(c => c === 2 || c === 3 || c === 4 || c === 7).length).reduce((a, b) => a + b),
+    part_1: T => P.prep(T).map(L => L[1].map(s => s.length).filter(c => c === 2 || c === 3 || c === 4 || c === 7).length).reduce((a, b) => a + b),
 
     findSegmentMapping: s => {
-        const w = s.sort((a, b) => a.length - b.length).map(w => w.split('').sort())
-        const k = Object.values(w.flatMap(v => v).reduce((o, c, i) => {
+        const w = s.sort((a, b) => a.length - b.length).map(w => w.split(''))
+        const k = Object.values(w.flatMap(v => v).reduce((o, c) => {
             if (o.hasOwnProperty(c))
                 o[c][0]++
             else
                 o[c] = [1, c]
             return o
-        }, []))
-        const r = [
+        }, [])).reduce((o, a) => { o[a[0]].push(a[1]); return o }, [0, 0, 0, 0, [], 0, [], [], [], []])
+        return [
             w[1].filter(n => !w[0].includes(n))[0],
-            k.filter(n => n[0] === 6)[0][1],
-            0,
-            0,
-            k.filter(n => n[0] === 4)[0][1],
-            k.filter(n => n[0] === 9)[0][1],
-            0
+            k[6][0],
+            w[0].filter(n => n !== k[9][0])[0],
+            k[7].filter(n => w[2].includes(n))[0],
+            k[4][0],
+            k[9][0],
+            k[7].filter(n => !w[2].includes(n))[0]
         ]
-        r[2] = w[0].filter(n => n !== r[5])[0]
-        const c7 = k.filter(n => n[0] === 7).map(n => n[1])
-        r[3] = c7.filter(n => w[2].includes(n))[0]
-        r[6] = c7.filter(n => !w[2].includes(n))[0]
-        return r
     },
 
     findDigit: (w, r) => {
