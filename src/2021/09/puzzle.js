a0572f8b6e3d0654ed54acab16b0e5d66fe0425e
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2021-12-09 21:19:57 +0100

    2021/09 functional optimizations

--- src/2021/09/puzzle.js
+++ src/2021/09/puzzle.js
@@ -1,37 +1,15 @@
 exports.puzzle = P = {
 
-    prep: T => T.trim().split('\n').map(L => L.split('').map(n => n * 1)),
+    prep: T => P.addBorder(T.trim().split('\n').map(L => L.split('').map(n => n * 1)), 1, 10),
 
-    prepEmpty: d => new Array(d.y).fill().map(y => new Array(d.x).fill(100)),
+    addBorder: (p, o, d) =>
+        new Array(p.length + (o << 1)).fill().map((_, y) =>
+            new Array(p[0].length + (o << 1)).fill().map((_, x) =>
+                p[y - o] !== undefined && p[y - o][x - o] !== undefined ? p[y - o][x - o] : d)),
 
-    populate: (dim, p) => {
-        const board = P.prepEmpty(dim)
-        const o = 1
-        for (let y = 0; y < p.length; y++) {
-            for (let x = 0; x < p[y].length; x++) {
-                board[o + y][o + x] = p[y][x]
-            }
-        }
-        return board
-    },
+    toString: b => b.reduce((o, y) => o + y.reduce((o, x) => o + x + ' ', '') + '\n', ''),
 
-    dump: () => {
-        let s = ''
-        for (p of P.board) {
-            for (let y = 1; y < P.D.y - 1; y++) {
-                for (let x = 1; x < P.D.x - 1; x++) {
-                    s += ' ' + p[y][x]
-                }
-                s += '\n'
-            }
-            s += '\n'
-        }
-        console.log(s)
-    },
-
-    clone: board => board.slice().map(y => y.slice()),
-
-    count: y => y.reduce((b, x) => b + x.filter(c => c === '#').length, 0),
+    clone: b => b.slice().map(y => y.slice()),
 
     lowPoint: (b, y, x) =>
         b[y][x] < b[y - 1][x] &&
@@ -41,77 +19,44 @@ exports.puzzle = P = {
             ? b[y][x] + 1
             : 0,
 
-    isBasin: (b, y, x) =>
-        b[y][x] < 9
-            ? 1
-            : 0,
-
-    lowPoints: () => {
-        const p = P.board[0]
-        const p1 = P.clone(p)
-        P.board.push(p1)
-        let s = 0
-        let low = []
-        for (let y = 1; y < P.D.y - 1; y++)
-            for (let x = 1; x < P.D.x - 1; x++) {
-                p1[y][x] = P.lowPoint(p, y, x)
-                s += p1[y][x]
-                if (p1[y][x] > 0)
-                    low.push({ x: x, y: y, z: p[y][x] })
+    lowPoints: p => {
+        const r = { sum: 0, low: [] }
+        for (let y = 1; y < p.length - 1; y++)
+            for (let x = 1; x < p[y].length - 1; x++) {
+                const l = P.lowPoint(p, y, x)
+                if (l > 0) {
+                    r.sum += l
+                    r.low.push({ x: x, y: y, z: l })
+                }
             }
-        return { low: low, s: s }
+        return r
     },
 
-    fillRek: (p,p1,c,h) => {
-        //console.log(c)
+    fillRek: (p, p1, c, h) => {
         if (p1[c.y][c.x] >= h)
             return 0
         p1[c.y][c.x] = h
-        c.s = 1
-            + P.fillRek(p,p1, { x: c.x+1, y: c.y }, h)
-            + P.fillRek(p,p1, { x: c.x-1, y: c.y }, h)
-            + P.fillRek(p,p1, { x: c.x, y: c.y-1 }, h)
-            + P.fillRek(p,p1, { x: c.x, y: c.y+1 }, h)
-        return c.s
+        return c.s = 1
+            + P.fillRek(p, p1, { x: c.x + 1, y: c.y }, h)
+            + P.fillRek(p, p1, { x: c.x - 1, y: c.y }, h)
+            + P.fillRek(p, p1, { x: c.x, y: c.y - 1 }, h)
+            + P.fillRek(p, p1, { x: c.x, y: c.y + 1 }, h)
     },
 
-    fillTo: (l, h) => {
-        const p = P.board[0]
-        const p1 = P.clone(p)
-        P.board.push(p1)
-        const r = l.filter(c => c.z<h).reduce((s,c) => {
-            return s + P.fillRek(p,p1,c,h)
-        }, 0)
-        return l
-    },
+    fillTo: (p, h) => P
+        .lowPoints(p).low
+        .filter(c => c.z < h)
+        .map(c => {
+            P.fillRek(p, P.clone(p), c, h)
+            return c
+        })
+        // count for part 2 result
+        .sort((a, b) => b.s - a.s)
+        .slice(0, 3)
+        .reduce((o, c) => o * c.s, 1),
 
-    part_1: T => {
-        const p = P.prep(T)
-        //console.log(p)
-        P.D = {
-            y: p.length + 2,
-            x: p[0].length + 2
-        }
-        const board = P.populate(P.D, p)
-        P.board = [P.clone(board)]
-        const s = P.lowPoints()
-        return s.s
-    },
+    part_1: T => P.lowPoints(P.prep(T)).sum,
 
-    part_2: T => {
-        const p = P.prep(T)
-        //console.log(p)
-        P.D = {
-            y: p.length + 2,
-            x: p[0].length + 2
-        }
-        const board = P.populate(P.D, p)
-        P.board = [P.clone(board)]
-        const s = P.lowPoints()
-        const r = P.fillTo(s.low, 9)
-        const l = r.sort((a,b) => b.s-a.s).slice(0,3).reduce((o,c) => o*c.s, 1)
-        //P.dump()
-        return l
-    }
+    part_2: T => P.fillTo(P.prep(T), 9)
 
 }
