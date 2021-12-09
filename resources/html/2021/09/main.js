74005fefd1aa7f8a45a0026f8eea3e6ddc125a7f
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2021-12-09 22:49:23 +0100

    2021/09 code formatting

--- resources/html/2021/09/main.js
+++ resources/html/2021/09/main.js
@@ -1,77 +1,77 @@
 const P = {
 
-    prep: T => P.addBorder(T.split('\n').map(L => L.split('').map(n => n * 1)), 1, 10),
+	prep: T => P.addBorder(T.split('\n').map(L => L.split('').map(n => n * 1)), 1, 10),
 
-    addBorder: (p, o, d) =>
-        new Array(p.length + (o << 1)).fill().map((_, y) =>
-            new Array(p[0].length + (o << 1)).fill().map((_, x) =>
-                p[y - o] !== undefined && p[y - o][x - o] !== undefined ? p[y - o][x - o] : d)),
+	addBorder: (p, o, d) =>
+		new Array(p.length + (o << 1)).fill().map((_, y) =>
+			new Array(p[0].length + (o << 1)).fill().map((_, x) =>
+				p[y - o] !== undefined && p[y - o][x - o] !== undefined ? p[y - o][x - o] : d)),
 
-    clone: b => b.slice().map(y => y.slice()),
-    // keep for debug purposes
-    toString: b => b.reduce((o, y) => o + y.reduce((o, x) => o + x + ' ', '') + '\n', ''),
+	clone: b => b.slice().map(y => y.slice()),
+	// keep for debug purposes
+	toString: b => b.reduce((o, y) => o + y.reduce((o, x) => o + x + ' ', '') + '\n', ''),
 
-    lowPoint: (b, y, x) =>
-        b[y][x] < b[y - 1][x] &&
-            b[y][x] < b[y + 1][x] &&
-            b[y][x] < b[y][x - 1] &&
-            b[y][x] < b[y][x + 1]
-            ? b[y][x] + 1
-            : 0,
+	lowPoint: (b, y, x) =>
+		b[y][x] < b[y - 1][x] &&
+			b[y][x] < b[y + 1][x] &&
+			b[y][x] < b[y][x - 1] &&
+			b[y][x] < b[y][x + 1]
+			? b[y][x] + 1
+			: 0,
 
-    lowPoints: p => {
-        const r = { sum: 0, low: [] }
-        for (let y = 1; y < p.length - 1; y++)
-            for (let x = 1; x < p[y].length - 1; x++) {
-                const l = P.lowPoint(p, y, x)
-                if (l > 0) {
-                    r.sum += l
-                    r.low.push({ x: x, y: y, z: l-1 })
-                }
-            }
-        return r
-    },
+	lowPoints: p => {
+		const r = { sum: 0, low: [] }
+		for (let y = 1; y < p.length - 1; y++)
+			for (let x = 1; x < p[y].length - 1; x++) {
+				const l = P.lowPoint(p, y, x)
+				if (l > 0) {
+					r.sum += l
+					r.low.push({ x: x, y: y, z: l - 1 })
+				}
+			}
+		return r
+	},
 
-    fillRek: (p, c, h) => {
-        if (p[c.y][c.x] >= h)
-            return 0
-        p[c.y][c.x] = h
-        return 1
-            + P.fillRek(p, { x: c.x + 1, y: c.y }, h)
-            + P.fillRek(p, { x: c.x - 1, y: c.y }, h)
-            + P.fillRek(p, { x: c.x, y: c.y - 1 }, h)
-            + P.fillRek(p, { x: c.x, y: c.y + 1 }, h)
-    },
+	fillRek: (p, c, h) => {
+		if (p[c.y][c.x] >= h)
+			return 0
+		p[c.y][c.x] = h
+		return 1
+			+ P.fillRek(p, { x: c.x + 1, y: c.y }, h)
+			+ P.fillRek(p, { x: c.x - 1, y: c.y }, h)
+			+ P.fillRek(p, { x: c.x, y: c.y - 1 }, h)
+			+ P.fillRek(p, { x: c.x, y: c.y + 1 }, h)
+	},
 
-    fillTo: (p, h) => P
-        .lowPoints(p).low
-        .filter(c => c.z < h)
-        .map(c => P.fillRek(p, c, h))
-        .sort((a, b) => b - a)
-        .slice(0, 3)
-        .reduce((o, s) => o * s, 1),
+	fillTo: (p, h) => P
+		.lowPoints(p).low
+		.filter(c => c.z < h)
+		.map(c => P.fillRek(p, c, h))
+		.sort((a, b) => b - a)
+		.slice(0, 3)
+		.reduce((o, s) => o * s, 1),
 
-    part_1: T => P.lowPoints(P.prep(T)).sum,
+	part_1: T => P.lowPoints(P.prep(T)).sum,
 
-    part_2: T => P.fillTo(P.prep(T), 9),
+	part_2: T => P.fillTo(P.prep(T), 9),
 
 	getData: () => P.board,
 
 	step: () => {
 		P.board[1] = P.clone(P.board[0])
-		P.level = (P.level+1) % 10
-        const r = P.fillTo(P.board[1], P.level)
-		if (P.level===9)
-			console.log(`part 2: ${r}`)		
+		P.level = (P.level + 1) % 10
+		const r = P.fillTo(P.board[1], P.level)
+		if (P.level === 9)
+			console.log(`part 2: ${r}`)
 	},
 
-    init: T => {
-        const p = P.prep(T)
+	init: T => {
+		const p = P.prep(T)
 		console.log(`part 1: ${P.lowPoints(p).sum}`)
 		P.board = [p, P.clone(p)]
 		P.level = -1
-        return P
-    }
+		return P
+	}
 
 }
 
@@ -88,7 +88,7 @@ class QuadModel {
 		this.v = []
 		this.c = []
 	}
-	
+
 	quadXM(x, y, z, c) {
 		c = c || this.col
 		const y1 = y + 1, z1 = z + 1, f = this.shade.xm, s = [f * c.r, f * c.g, f * c.b]
@@ -170,14 +170,14 @@ class Scene extends QuadModel {
 		const p1 = board[1]
 		const cg = { r: 1, g: 0.7, b: 0.5 }
 		const cw = { r: 0, g: 0.784, b: 1 }
-		const dz = p.length, dz2 = dz >>1 
+		const dz = p.length, dz2 = dz >> 1
 		for (let z = 0; z < dz; z++) {
 			for (let x = 0; x < p[z].length; x++) {
-				if (p[z][x]<10) {
-					this.cubeAt(x - (p[z].length>>1), p[z][x], z-dz2, cg)
-					if (p[z][x]<p1[z][x])
-						for (let h=0; h<p1[z][x]-p[z][x]; h++)
-							this.cubeAt(x - (p[z].length>>1), p1[z][x]-h, z-dz2, cw)
+				if (p[z][x] < 10) {
+					this.cubeAt(x - (p[z].length >> 1), p[z][x], z - dz2, cg)
+					if (p[z][x] < p1[z][x])
+						for (let h = 0; h < p1[z][x] - p[z][x]; h++)
+							this.cubeAt(x - (p[z].length >> 1), p1[z][x] - h, z - dz2, cw)
 				}
 			}
 		}
