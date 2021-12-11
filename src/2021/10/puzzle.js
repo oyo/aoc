7cc84895bf6b26bd0753f261b032b9073ee07fca
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2021-12-11 20:29:37 +0100

    2021/10 optimizations

--- src/2021/10/puzzle.js
+++ src/2021/10/puzzle.js
@@ -9,36 +9,32 @@ exports.puzzle = P = {
         '>': 25137
     },
 
-    score: s => s.split('').reduce((o,c) => o * 5 + ' )]}>'.indexOf(c), 0),
+    score: s => s.split('').reduce((o, c) => o * 5 + ' )]}>'.indexOf(c), 0),
 
     checkLine: l => {
-        let s
+        /*
         do {
             s = l.length
-            l = l.replace(/\[\]/g,'').replace(/\(\)/g,'').replace(/\<\>/g,'').replace(/\{\}/g,'')
+            l = l.replace(/(\[\]|\(\)|\<\>|\{\})/g, '')
         } while (l.length < s)
         return l;
+        */
+        while (l.length < (l = l.replace(/(\[\]|\(\)|\<\>|\{\})/g, '')).length)
+            ;
+        return l
     },
 
-    part_1: T => {
-        const p = P.prep(T)
-        const r = p.map(l => P.checkLine(l).replace(/[\(\[\<\{]/g, '')).filter(l => l.match(/[\)\]\>\}]/)).map(l => P.score1[l[0]]).reduce((a,b) => a+b)
-        return r
-    },
+    part_1: T => P.prep(T)
+        .map(l => P.checkLine(l).replace(/[\(\[\<\{]/g, ''))
+        .filter(l => l.match(/[\)\]\>\}]/))
+        .map(l => P.score1[l[0]])
+        .reduce((a, b) => a + b),
 
-    part_2: T => {
-        const p = P.prep(T)
-        const r = p.map(l => P.checkLine(l)).filter(l => !l.match(/[\)\]\>\}]/))
-        const s = r.map(l => {
-            const a = '([{<'
-            const b = ')]}>'
-            let d = ''
-            for (let c=0; c<l.length; c++) {
-                d = b[a.indexOf(l[c])]+d
-            }
-            return d
-        }).map(l => P.score(l)).sort((a,b) => a-b)[r.length>>1]
-        return s
-    }
+    part_2: T => (r => r.map(l => P.score(
+        l.split('').reduce((o, c) => ')]}>'['([{<'.indexOf(c)] + o, ''))
+    ).sort((a, b) => a - b)[r.length >> 1]
+    )(
+        P.prep(T).map(l => P.checkLine(l)).filter(l => !l.match(/[\)\]\>\}]/))
+    )
 
 }
