bcd98a739c626f1ed2f6f1e55e7ac24afbea6917
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2021-12-12 15:29:23 +0100

    2021/12 not optimized

---
+++ src/2021/12/puzzle.test.js
@@ -0,0 +1,54 @@
+const { puzzle } = require('./puzzle')
+
+const INPUT1 = 
+`start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end`
+
+const INPUT2= 
+`dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc`
+
+const INPUT3= 
+`fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW`
+
+it('solves 1', () => {
+    expect(puzzle.part_1(INPUT1)).toEqual(10)
+    expect(puzzle.part_1(INPUT2)).toEqual(19)
+    expect(puzzle.part_1(INPUT3)).toEqual(226)
+})
+
+it('solves 2', () => {
+    //expect(puzzle.part_2(INPUT1)).toEqual(36)
+    expect(puzzle.part_2(INPUT2)).toEqual(103)
+    //expect(puzzle.part_2(INPUT3)).toEqual(3509)
+})
