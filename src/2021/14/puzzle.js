6486517928981affe01ff8640b3158eaa07ed968
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2021-12-14 14:13:39 +0100

    2021/14 minor optimizations

--- src/2021/14/puzzle.js
+++ src/2021/14/puzzle.js
@@ -18,7 +18,7 @@ exports.puzzle = P = {
             }
             return o
         }, {})
-        q.c = T.replace(/[\s->]/g, '').split('').sort().reduce((o, c) => {
+        q.c = T.replace(/[\s->]/g, '').split('').reduce((o, c) => {
             if (!o.hasOwnProperty(c))
                 o[c] = new Array(n + 1).fill(0)
             o[c][0] = (q.s.match(new RegExp(c, 'g')) || []).length
@@ -27,14 +27,10 @@ exports.puzzle = P = {
         return q
     },
 
-    step: (p, n) => Object.keys(p.o).forEach(k => {
-        p.o[p.o[k].n[0]].c[n + 1] += p.o[k].c[n]
-        p.o[p.o[k].n[1]].c[n + 1] += p.o[k].c[n]
-    }),
+    step: (o, n) => Object.keys(o).map(k => [0, 1].map(l => o[o[k].n[l]].c[n + 1] += o[k].c[n])),
 
     count: (p, n) => Object.keys(p.o).reduce((o, k) => {
-        o[k[0]] = o.hasOwnProperty(k[0]) ? o[k[0]] + p.o[k].c[n] : p.o[k].c[n]
-        o[k[1]] = o.hasOwnProperty(k[1]) ? o[k[0]] + p.o[k].c[n] : p.o[k].c[n]
+        [0, 1].map(l => o[k[l]] = o[k[l]] + p.o[k].c[n])
         p.c[p.o[k].v][n + 1] += p.o[k].c[n]
         return o
     }, {}),
@@ -42,7 +38,7 @@ exports.puzzle = P = {
     run: (T, n) => {
         const p = P.prep(T, n)
         for (let i = 0; i < n; i++) {
-            P.step(p, i)
+            P.step(p.o, i)
             P.count(p, i)
         }
         const s = Object.keys(p.c).map(k => [k, p.c[k].reduce((a, b) => a + b)]).sort((a, b) => a[1] - b[1])
