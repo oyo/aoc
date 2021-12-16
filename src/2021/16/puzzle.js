e6c6eabc2307e7e3bca744b8a188bb9dc8d136f9
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2021-12-16 16:18:15 +0100

    2021/16 solution

---
+++ src/2021/16/puzzle.js
@@ -0,0 +1,9 @@
+const Packet = require('./Packet')
+
+exports.puzzle = P = {
+
+    part_1: T =>  new Packet(T.trim()).calcVersionSum(),
+
+    part_2: T =>  new Packet(T.trim()).calcOperation()
+
+}
