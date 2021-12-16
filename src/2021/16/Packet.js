c1e78012281f1ae84230c4e55d08d01cd0c9edf9
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2021-12-16 16:18:15 +0100

    2021/16 solution

---
+++ src/2021/16/Packet.js
@@ -0,0 +1,112 @@
+Object.assign(String.prototype, {
+    splitAt(n) {
+        return [this.slice(0, n), this.slice(n)]
+    }
+})
+
+class Packet {
+
+    constructor(hex, pointer) {
+        this.hex = hex
+        this.data = BigInt('0x'+hex)
+        this.length = BigInt(hex.length << 2)
+        this.pointer = pointer ? BigInt(pointer) : 0n
+        this.children = []
+        this.readVersionType()
+        this.readValue()
+    }
+
+    readVersionType() {
+        this.version = Number(this.nextToken(3))
+        this.typeid = Number(this.nextToken(3))
+    }
+
+    readValue() {
+        if (this.typeid === 4)
+            return this.readLiteral()
+        else
+            return this.readOperator()
+    }
+
+    readLiteral() {
+        let token
+        let value = 0n
+        let i = 0n
+        do {
+            token = this.nextToken(5)
+            value = (value << 4n) | (token & 0b1111n)
+            i++
+        } while (token > 0b1111n && i<10)
+        this.value = value
+    }
+
+    readOperator() {
+        if (this.nextToken(1)) {
+            const npkgs = this.nextToken(11)
+            for (let p = 0; p < npkgs; p++) {
+                this.readSub()
+            }
+        } else {
+            const nbits = this.nextToken(15)
+            const target = this.pointer + nbits
+            for (let p = this.pointer; this.pointer < target;) {
+                this.readSub()
+            }
+        }
+    }
+
+    readSub() {
+        const child = new Packet(this.hex, this.pointer)
+        this.pointer = child.pointer
+        this.children.push(child)
+    }
+
+    pad(s, l) {
+        return (new Array(Number(l)).fill(0).join('') + s).slice(-Number(l))
+    }
+
+    toBinary(b, l) {
+        return `${this.pad(b.toString(2), l)}`
+    }
+
+    toStringDebug() {
+        return `${this.length}->${this.pointer}: ${this.toBinary(this.data, this.length).splitAt(Number(this.pointer)).join('.')} ${this.children}`
+    }
+
+    toString() {
+        return this.typeid === 4
+            ? `LI ${this.value} v${this.version}`
+            : `OP ${this.typeid} v${this.version} [ ${this.children} ]`
+    }
+
+    calcVersionSum() {
+        return this.version + this.children.reduce((o, a) => o + a.calcVersionSum(), 0)
+    }
+
+    calcOperation() {
+        switch (this.typeid) {
+            case 0: return this.children.reduce((o, a) => o + a.calcOperation(), 0n)
+            case 1: return this.children.reduce((o, a) => o * a.calcOperation(), 1n)
+            case 2: return BigInt(Math.min(...this.children.map(c => Number(c.calcOperation()))))
+            case 3: return BigInt(Math.max(...this.children.map(c => Number(c.calcOperation()))))
+            case 4: return this.value
+            case 5: return this.children[0].calcOperation() > this.children[1].calcOperation() ? 1n : 0n
+            case 6: return this.children[0].calcOperation() < this.children[1].calcOperation() ? 1n : 0n
+            case 7: return this.children[0].calcOperation() === this.children[1].calcOperation() ? 1n : 0n
+        }
+        return 0n
+    }
+
+    nextToken(b) {
+        const data = this.data
+        const bits = BigInt(b)
+        const mask = (1n << bits) - 1n
+        const right = this.length - this.pointer - bits
+        const token = (data >> right) & mask
+        this.pointer += bits
+        return token
+    }
+
+}
+
+module.exports = Packet
---
+++ src/2021/16/Packet.test.js
@@ -0,0 +1,53 @@
+const Packet = require('./Packet')
+
+const LITERAL = `D2FE28`
+const OPERATOR_0 = `38006F45291200`
+const OPERATOR_1 = `EE00D40C823060`
+
+const VERSIONS = {
+    '8A004A801A8002F478': 16,
+    '620080001611562C8802118E34': 12,
+    'C0015000016115A2E0802F182340': 23,
+    'A0016C880162017C3686B18A3D4780': 31
+}
+
+const OPS = {
+    'C200B40A82': 3n,
+    '04005AC33890': 54n,
+    '880086C3E88112': 7n,
+    'CE00C43D881120': 9n,
+    'D8005AC2A8F0': 1n,
+    'F600BC2D8F': 0n,
+    '9C005AC2F8F0': 0n,
+    '9C0141080250320F1802104A08': 1n
+}
+
+/*
+it('parses literals', () => {
+    const li = new Packet(LITERAL)
+    expect(li.version).toEqual(6)
+    expect(li.typeid).toEqual(4)
+    expect(li.value).toEqual(2021n)
+})
+
+it('parses operator 0', () => {
+    const op = new Packet(OPERATOR_0)
+    expect(op.version).toEqual(1)
+    expect(op.typeid).toEqual(6)
+    expect(op.children.length).toEqual(2)
+})
+
+it('parses operator 1', () => {
+    const op = new Packet(OPERATOR_1)
+    expect(op.version).toEqual(7)
+    expect(op.typeid).toEqual(3)
+    expect(op.children.length).toEqual(3)
+})
+
+it('calculates version sums', () => Object.entries(VERSIONS).forEach(p =>
+    expect(new Packet(p[0]).calcVersionSum()).toEqual(p[1]))
+)
+*/
+it('calculates operations', () => Object.entries(OPS).forEach(p =>
+    expect(new Packet(p[0]).calcOperation()).toEqual(p[1]))
+)
