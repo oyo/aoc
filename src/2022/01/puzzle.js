933220486cf86fbccfee5d4c0166778b3b0ea403
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2022-12-01 06:53:59 +0100

    2022/01 solution

---
+++ src/2022/01/puzzle.js
@@ -0,0 +1,9 @@
+exports.puzzle = P = {
+
+    prep: T => T.split('\n\n').map(L => L.split('\n').reduce((a, c) => a + Number.parseInt(c), 0)),
+
+    part_1: T => Math.max.apply(null, P.prep(T)),
+
+    part_2: T => P.prep(T).sort((a, b) => b - a).slice(0, 3).reduce((a, c) => a + c, 0)
+
+}
