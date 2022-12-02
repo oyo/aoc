7caf6c2e3627e85f94129499b677bf2e5c5241f2
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2022-12-02 09:48:04 +0100

    2022/02 simplified

--- src/2022/02/puzzle.js
+++ src/2022/02/puzzle.js
@@ -2,7 +2,7 @@ exports.puzzle = P = {
 
     prep: T => T.split('\n').map(L => [L.charCodeAt(0) - 64, L.charCodeAt(2) - 87]),
 
-    score: (p) => p.map(r => r[0] + r[1] * 3).reduce((a, b) => a + b),
+    score: p => p.map(r => r[0] + r[1] * 3).reduce((a, b) => a + b),
 
     part_1: T => P.score(P.prep(T).map(L => [L[1], (L[1] - L[0] + 4) % 3])),
 
