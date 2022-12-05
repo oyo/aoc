7d495979ac50ebb2e3782ae9154770a85128ddb2
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2022-12-05 10:02:54 +0100

    2022/05 simpified

--- src/2022/05/puzzle.js
+++ src/2022/05/puzzle.js
@@ -2,57 +2,42 @@ exports.puzzle = P = {
 
     prep: T => T.split('\n\n').map(L => L.split('\n')),
 
-    parseCrates: C => C.map(c => c.match(/.{3,4}/g).map(d => d.replace(/[ \[\]]/g,''))),
+    parseCrates: C => C.map(c => c.match(/.{3,4}/g).map(d => d.replace(/[ \[\]]/g, ''))),
 
     transposeCrates: C => {
-        const o = C[0].map(c => new Array(C.length).fill(''))
-        for (let c=0; c < o.length; c++) {
-            for (let r = C.length-1; r>=0; r--) {
+        const o = C[0].map(c => [])
+        for (let c = 0; c < o.length; c++)
+            for (let r = C.length - 1; r >= 0; r--)
                 o[c][r] = C[r][c]
-            }
-        }
-        return o.map(D => D.reverse().filter(E => E !== ''))
+        return o.map(D => D.reverse().filter(E => E))
     },
 
-    parseMoves: M => M.map(m => m.split(/(move | from | to )/)).filter(n => n.length>6).map(n => [n[2], n[4], n[6]].map(o => Number.parseInt(o))),
+    parseMoves: M => M.map(m => m.split(/(move | from | to )/))
+        .filter(n => n.length > 6)
+        .map(n => [n[2], n[4], n[6]].map(o => Number.parseInt(o))),
 
-    move: (C, M) => {
-        const n = M[0]
-        for (let i=0; i<n; i++) {
-            C[M[2]-1].push(C[M[1]-1].pop())
-        }
+    move1: (C, M) => {
+        for (let i = 0; i < M[0]; i++)
+            C[M[2] - 1].push(C[M[1] - 1].pop())
         return C
     },
 
-    move2: (C, M) => {
+    moveN: (C, M) => {
         const n = M[0]
-        const s = M[1]-1
-        let t = M[2]-1
-        const mc = C[s].splice(C[s].length-n,n)
-        C[t] = C[t].concat(mc)
+        const s = M[1] - 1
+        const t = M[2] - 1
+        C[t] = C[t].concat(C[s].splice(C[s].length - n, n))
         return C
     },
 
-    part_1: T => {
-        const p = P.prep(T)
-        const d = P.parseCrates(p[0].slice(0, p[0].length-1))
-        let c = P.transposeCrates(d)
-        const m = P.parseMoves(p[1])
-        for (let i=0; i<m.length; i++)
-            c = P.move(c, m[i])
-        const r = c.map(d => d.pop())
-        return r.join('')
-    },
+    process: (T, move) => (([c, m]) => {
+        const d = P.transposeCrates(P.parseCrates(c.slice(0, c.length - 1)))
+        P.parseMoves(m).forEach(n => move(d, n))
+        return d.map(d => d.pop()).join('')
+    })(P.prep(T)),
 
-    part_2: T => {
-        const p = P.prep(T)
-        const d = P.parseCrates(p[0].slice(0, p[0].length-1))
-        let c = P.transposeCrates(d)
-        const m = P.parseMoves(p[1])
-        for (let i=0; i<m.length; i++)
-            c = P.move2(c, m[i])
-        const r = c.map(d => d.pop())
-        return r.join('')
-    }
+    part_1: T => P.process(T, P.move1),
+
+    part_2: T => P.process(T, P.moveN)
 
 }
