c1ec82fb9a0d912129d78a37f53947563bea7005
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2023-12-03 03:37:05 +0100

    23/02 solution

---
+++ src/2023/02/puzzle.test.js
@@ -0,0 +1,12 @@
+const { puzzle } = require('./puzzle')
+
+const INPUT = 
+`Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
+
+it('solves 1', () => expect(puzzle.part_1(INPUT)).toEqual(8))
+
+it('solves 2', () => expect(puzzle.part_2(INPUT)).toEqual(2286))
