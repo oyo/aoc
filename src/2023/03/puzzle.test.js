94f46b46c3c7adafcf885a5cb7c0f34149d38d74
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2023-12-04 03:53:15 +0100

    yarn upgrade

--- src/2023/03/puzzle.test.js
+++ src/2023/03/puzzle.test.js
@@ -12,6 +12,6 @@ const INPUT =
 ...$.*....
 .664.598..`
 
-//it('solves 1', () => expect(puzzle.part_1(INPUT)).toEqual(4361))
+it('solves 1', () => expect(puzzle.part_1(INPUT)).toEqual(4361))
 
 it('solves 2', () => expect(puzzle.part_2(INPUT)).toEqual(467835))
