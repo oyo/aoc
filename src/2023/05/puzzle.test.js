b476af4f7460aeb86f509608fba72871cebe94b3
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2023-12-05 04:33:02 +0100

    23/04 remove output and add default N

--- src/2023/05/puzzle.test.js
+++
@@ -1,8 +0,0 @@
-const { puzzle } = require('./puzzle')
-
-const INPUT = 
-``
-
-it('solves 1', () => expect(puzzle.part_1(INPUT)).toEqual(9))
-
-//it('solves 2', () => expect(puzzle.part_2(INPUT)).toEqual(2))
