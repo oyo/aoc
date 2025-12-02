9e6600b09744b46a06804745bff0bb972cca48fb
Martin Rohrmeier <roryphant@gmail.com>
2025-12-02 03:12:51 +0100

    25/01 improved

--- src/2025/01/puzzle.js
+++ src/2025/01/puzzle.js
@@ -4,29 +4,24 @@ exports.puzzle = P = {
 
     prep: T => T.trim().replaceAll('L', '-').replaceAll('R', '').split('\n').map(N),
 
-    part_1: T => {
-        let z = 0
-        P.prep(T).reduce((a, c) => {
-            const n = (a + c) % 100
-            if (n === 0) z++
-            return n
-        }, 50)
-        return z
+    stop0: (a, c) => {
+        if ((a[0] = (a[0] + c) % 100) === 0)
+            a[1]++
+        return a
     },
 
-    part_2: T => {
-        const p = P.prep(T)
-        let a = 50
-        let z = 0
-        for (let c of p) {
-            z += ~~(Math.abs(c) / 100)
-            c = c % 100
-            for (let i = 0; i < Math.abs(c); i++) {
-                a += c > 0 ? 1 : -1
-                if (a % 100 === 0) z++
-            }
-        }
-        return z
-    }
+    pass0: (a, c) => {
+        a[1] += ~~(Math.abs(c) / 100)
+        for (let i = 0; i < Math.abs(c % 100); i++)
+            if ((a[0] += Math.sign(c)) % 100 === 0)
+                a[1]++
+        return a
+    },
+
+    run: (T, r) => P.prep(T).reduce(r, [50, 0])[1],
+
+    part_1: T => P.run(T, P.stop0),
+
+    part_2: T => P.run(T, P.pass0)
 
 }
