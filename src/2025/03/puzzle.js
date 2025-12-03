18235733253940c6011ffbdb1bc105a7488e2453
Martin Rohrmeier <roryphant@gmail.com>
2025-12-03 08:05:40 +0100

    25/03 cleanup mess

--- src/2025/03/puzzle.js
+++ src/2025/03/puzzle.js
@@ -4,42 +4,27 @@ exports.puzzle = P = {
 
     prep: T => T.trim().split('\n').map(L => L.split('').map(N)),
 
-    fv1: b => {
-        const o = b.filter((v, i, a) => a.indexOf(v) === i).sort((a, b) => b - a)
-        let m = o[0]
-        let i = b.indexOf(m)
-        if (i === b.length - 1) {
-            m = o[1]
-            i = b.indexOf(m)
-        }
-        const r = b.slice(i + 1)
-        const n = Math.max(...r)
-        return m * 10 + n
-    },
-
-    fv2: b => {
-        let o = [...b].sort((a, b) => b - a)
-        let c = [...b]
-        let d = []
-        let oi = 0
-        while (d.length < 12) {
+    jolt: (l, b) => {
+        const d = []
+        for (
+            let o = [...b].sort((a, b) => b - a), oi = 0;
+            d.length < l;
+            oi++
+        ) {
             const n = o[oi]
-            const i = c.indexOf(n)
-            if (i >= 0 && i < c.length - (11 - d.length)) {
+            const i = b.indexOf(n)
+            if (i >= 0 && i < b.length - (l - d.length - 1)) {
                 d.push(n)
                 o.splice(oi, 1)
-                c = c.slice(i + 1)
-                oi = 0
-            } else
-                oi++
+                b = b.slice(i + 1)
+                oi = -1
+            }
         }
         return N(d.join(''))
     },
 
-    run: (T, R) => P.prep(T).reduce((a, c) => a + R(c), 0),
+    part_1: T => P.prep(T).reduce((a, c) => a + P.jolt(2, c), 0),
 
-    part_1: T => P.run(T, P.fv1),
-
-    part_2: T => P.run(T, P.fv2)
+    part_2: T => P.prep(T).reduce((a, c) => a + P.jolt(12, c), 0)
 
 }
