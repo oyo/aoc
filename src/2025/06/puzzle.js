efefbacec4dea8a97e8100149d4ab963fff2060c
Martin Rohrmeier <roryphant@gmail.com>
2025-12-06 08:37:08 +0100

    25/06 reuse reduce

--- src/2025/06/puzzle.js
+++ src/2025/06/puzzle.js
@@ -1,36 +1,32 @@
 const N = n => Number.parseInt(n)
-const transN = a => a[0].map((_, c) => a.map(r => N(r[c])))
+const traN = a => a[0].map((_, c) => a.map(r => N(r[c])))
+const rop = (p, o) => o === '+'
+    ? p.reduce((a, c) => a + c, 0)
+    : p.reduce((a, c) => a * c, 1)
 
 exports.puzzle = P = {
 
     part_1: T => (
         p => (
-            (op, q) => op.reduce((ao, co, i) =>
-                ao + (
-                    co === '+'
-                        ? q[i].reduce((a, c) => a + c, 0)
-                        : q[i].reduce((a, c) => a * c, 1)
-
-                ), 0)
+            (o, q) => o.reduce((a, c, i) => a + rop(q[i], c), 0)
         )(
-            p.pop(), transN(p)
-        ))(
-            T.trim().split('\n').map(L => L.trim().split(/ +/))
-        ),
+            p.pop(), traN(p)
+        )
+    )(
+        T.trim().split('\n').map(L => L.trim().split(/ +/))
+    ),
 
     part_2: T => {
         let p = T.split('\n').slice(0, -1)
-        let op = p.pop()
+        let o = p.pop()
         if (p.length < 4)
-            p.push(new Array(op.length + 1).join(' '))
+            p.push(new Array(o.length + 1).join(' '))
         let s = 0
-        for (let o = [], i = op.length - 1; i >= 0; i--) {
-            o.push(N(p[0][i] + p[1][i] + p[2][i] + p[3][i]))
-            op[i] !== ' ' && (
-                s += op[i] === '+'
-                    ? o.reduce((a, c) => a + c, 0)
-                    : o.reduce((a, c) => a * c, 1),
-                o = [],
+        for (let r = [], i = o.length - 1; i >= 0; i--) {
+            r.push(N(p[0][i] + p[1][i] + p[2][i] + p[3][i]))
+            o[i] !== ' ' && (
+                s += rop(r, o[i]),
+                r = [],
                 i--
             )
         }
